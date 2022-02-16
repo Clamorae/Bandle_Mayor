26d06092fc97557b26e95dae4e3d6cb93247e401
Clamore <rotuman.lenoob@gmail.com>
2022-02-16 01:28:08 +0100

    I'm dumb

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const { Client, Intents } = require('discord.js');
-const sql = require("sqlite");
-sql.open("./database.sqlite")
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 require("dotenv").config();
 
